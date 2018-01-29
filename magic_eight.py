5e6ff49de83c5b3212e64e070a4657fb3a3038f9
Anndo Ko <handcuffanndo@gmail.com>
2018-01-29 13:56:32 -0500

    add a while loop & check user_input

--- magic_eight.py
+++ magic_eight.py
@@ -1,8 +1,12 @@
 # Create a function that asks users “What is your question?” (or something similar) and saves the response.
-
 def prompt():
-    question = input("What is your question?")
-    return question
+    user_question = input("Please enter your question or enter 'quit' to end the program.\n")
+    return user_question
 
-q = prompt()
-print(q)
+user_input = ""
+while user_input != "quit":
+    user_input = prompt()
+
+    # checks if user input is a question (i.e., ends in a ‘?’) and, if not, prints “I’m sorry, I can only answer questions.”
+    if user_input[-1] != "?":
+        print("I’m sorry, I can only answer questions.")
