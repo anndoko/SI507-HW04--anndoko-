20e8c39700e81f9015c394f24a5744a0d259d614
Xinyu Yan <yanxinyu@0587358875.wireless.umich.net>
2018-01-29 13:46:55 -0500

    add branch

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,5 @@
+import random
+
 # Create a function that asks users “What is your question?” (or something similar) and saves the response.
 
 def prompt():
@@ -6,3 +8,29 @@ def prompt():
 
 q = prompt()
 print(q)
+
+magic_ans = [
+"It is certain",
+"It is decidedly so",
+"Without a doubt",
+"Yes definitely",
+"You may rely on it",
+"As I see it, yes",
+"Most likely",
+"Outlook good",
+"Yes",
+"Signs point to yes",
+"Reply hazy try again",
+"Ask again later",
+"Better not tell you now",
+"Cannot predict now",
+"Concentrate and ask again",
+"Don't count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful",
+]
+
+answer = random.choice(magic_ans)
+print(answer)
