844830635b2f0772cb4b417c7ac004e23f42e6f9
Anndo Ko <handcuffanndo@gmail.com>
2018-01-29 13:37:50 -0500

    set up HW4

---
+++ magic_eight.py
@@ -0,0 +1,8 @@
+# Create a function that asks users “What is your question?” (or something similar) and saves the response.
+
+def prompt():
+    question = input("What is your question?")
+    return question
+
+q = prompt()
+print(q)
